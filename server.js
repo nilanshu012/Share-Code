539b36fd6d230e4d309a1388f460e3c91dca4e27
Nil <nil@local.com>
2023-10-07 01:47:25 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,63 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import "dotenv/config";
+
+const app= express();
+const port = process.env.PORT || 3000;
+// const localurl = 'mongodb://127.0.0.1:27017/code'; 
+const uri= process.env.uri;
+mongoose.connect(uri);
+
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+const dataSchema =new mongoose.Schema({
+    value:{
+        type : String,
+        required : true
+    }
+})
+
+const Data = mongoose.model('Data',dataSchema);
+
+
+app.get('/',(req,res)=>{
+    let data=`    kgfalk
+     gaSG `;
+    res.render('index.ejs',{code:data,language:'plaintext'});
+})
+
+app.get('/new',(req,res)=>{
+    res.render('new.ejs');
+})
+
+app.post('/save',async (req,res)=>{
+    
+    const userData= req.body.userdata;
+    try{
+        const data = await Data.create({value:userData});
+        res.redirect(`/${data.id}`);
+       // console.log(data.id);
+    }catch(error){
+        res.render('new.ejs',{code:userData});
+    }
+    //console.log(userData);
+
+})
+
+app.get('/:id',async (req,res)=>{
+    const id =req.params.id;
+
+    try{
+        const data = await Data.findById(id);
+        res.render('index.ejs',{code:data.value});
+    }catch(error){
+        res.redirect('/');
+    }
+
+})
+
+app.listen(port,()=>{
+    console.log(`Server running on ${port}`);
+})
